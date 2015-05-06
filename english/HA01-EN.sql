6b65e232c4877d673c3cfb424fd7a0fc14f3bf85
Jamezs Gladney <jamezs@angelofcode.com>
2015-05-06 18:48:40 -0400

    SQL Update

---
+++ english/HA01-EN.sql
@@ -0,0 +1,67 @@
+BEGIN TRANSACTION;
+INSERT OR REPLACE INTO "datas" VALUES (60161788,3,0,47,33,300,500,1,64,2,256);
+INSERT OR REPLACE INTO "texts" VALUES (60161788, 'Blizzed, Defender of the Ice Barrier', 'When this card is destroyed by battle and sent to the Graveyard, draw 1 card.', 'Draw card', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (96565487,3,0,0,33,1400,400,3,1,2,0);
+INSERT OR REPLACE INTO "texts" VALUES (96565487, 'Blizzard Warrior', 'If this card destroys an opponent''s monster by battle, look at the top card of their Deck, then return it to either the top or bottom of their Deck.', 'Confirm', 'Return to the top of the deck', 'Return to the bottom of the deck', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (23950192,3,0,47,4129,1300,0,2,64,2,65536);
+INSERT OR REPLACE INTO "texts" VALUES (23950192, 'Cryomancer of the Ice Barrier', 'While you control another "Ice Barrier" monster, Level 4 or higher monsters cannot declare an attack.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (69448290,3,0,55,33,1100,700,3,4096,8,327680);
+INSERT OR REPLACE INTO "texts" VALUES (69448290, 'Mist Valley Thunderbird', 'When this face-up card on the field returns to the hand, Special Summon it. This card cannot attack during the turn it was Special Summoned by this effect.', 'Special Summon', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (95443805,3,0,55,4129,1200,1200,3,512,8,8208);
+INSERT OR REPLACE INTO "texts" VALUES (95443805, 'Mist Valley Shaman', 'Once per turn, you can return 1 other monster you control to the hand, to have this card gain 500 ATK until the End Phase.', 'Gain attack', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (22837504,3,0,55,4129,1700,300,4,512,8,16);
+INSERT OR REPLACE INTO "texts" VALUES (22837504, 'Mist Valley Soldier', 'While you control this face-up card, any opponent''s monster that battles this card, but is not destroyed by battle, returns to its owner''s hand at the end of the Damage Step.', 'Return to hand', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (68226653,3,0,44,33,1100,200,2,8192,4,6291972);
+INSERT OR REPLACE INTO "texts" VALUES (68226653, 'Flamvell Dragnov', 'When this card is destroyed by battle and sent to the Graveyard, inflict 500 damage to your opponent. You can remove from play this card in your Graveyard, along with 1 face-up FIRE monster you control, to add 1 "Flamvell Dragnov" from your Deck to your hand.', '给予对方500伤害', 'Search', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (95621257,3,0,44,4129,1400,200,4,2,4,8192);
+INSERT OR REPLACE INTO "texts" VALUES (95621257, 'Flamvell Magician', 'While you control an "Ally of Justice" monster(s), this card gains 400 ATK.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (21615956,3,0,44,4113,100,2000,1,8192,4,0);
+INSERT OR REPLACE INTO "texts" VALUES (21615956, 'Flamvell Guard', 'A Flamvell guardian who commands fire with his will. His magma-hot barrier protects his troops from intruders.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (53100061,3,0,4109,33,400,100,1,32768,1,256);
+INSERT OR REPLACE INTO "texts" VALUES (53100061, 'X-Saber Axel', 'While you control this face-up card, when a face-up "Saber" monster you control is destroyed by battle and sent to the Graveyard, draw 1 card.', 'Draw card', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (90508760,3,0,856077,4129,1600,200,3,16384,1,64);
+INSERT OR REPLACE INTO "texts" VALUES (90508760, 'X-Saber Airbellum', 'If this card inflicts battle damage to your opponent by a direct attack: Discard 1 random card from your opponent''s hand.', 'Discard hand', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (26993374,3,0,4109,33,1600,1000,4,32768,1,32);
+INSERT OR REPLACE INTO "texts" VALUES (26993374, 'X-Saber Uruz', 'When this card destroys an opponent''s monster by battle and sends it to the Graveyard, you can Tribute this card to return the destroyed card to the top of its owner''s Deck.', 'Return to deck', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (53388413,3,0,176,33,2100,1600,6,32768,1,8192);
+INSERT OR REPLACE INTO "texts" VALUES (53388413, 'Commander Gottoms, Swordmaster', 'All face-up "Saber" monsters you control gain 400 ATK.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (89386122,3,0,1,17,2300,1200,6,32,32,0);
+INSERT OR REPLACE INTO "texts" VALUES (89386122, 'Ally of Justice Clausolas', 'A mighty monster in Mist Valley, and the ultimate weapon designed from Claiomh Solais. Its surprise attacks from above baffle invaders.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (25771826,3,0,1,33,1600,400,4,32,32,8192);
+INSERT OR REPLACE INTO "texts" VALUES (25771826, 'Ally of Justice Garadholg', 'If this card battles a LIGHT monster, it gains 200 ATK during the Damage Step only.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (52265835,3,0,1,33,1900,1200,5,32,32,2105344);
+INSERT OR REPLACE INTO "texts" VALUES (52265835, 'Ally of Justice Rudra', 'If this card battles a LIGHT monster, it gains 700 ATK during the Damage Step only.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (88650530,3,0,62,2097185,300,200,1,524288,16,1);
+INSERT OR REPLACE INTO "texts" VALUES (88650530, 'Worm Apocalypse', 'FLIP: Target 1 Spell/Trap Card on the field; destroy that target.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (15658249,3,0,62,33,1400,1500,3,524288,16,2048);
+INSERT OR REPLACE INTO "texts" VALUES (15658249, 'Worm Barses', 'When this card is Normal Summoned: Target 1 Defense Position monster; change that target to face-up Attack Position.', '变更表示形式', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (51043243,3,0,62,2097185,1200,500,4,524288,16,1049088);
+INSERT OR REPLACE INTO "texts" VALUES (51043243, 'Worm Cartaros', 'FLIP: Add 1 Level 4 or lower Reptile-Type "Worm" monster from your Deck to your hand.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (88438982,3,0,62,2097185,1700,1400,4,524288,16,8192);
+INSERT OR REPLACE INTO "texts" VALUES (88438982, 'Worm Dimikles', 'FLIP: This card gains 300 ATK and DEF.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (14936691,3,0,62,33,2400,1200,6,524288,16,2048);
+INSERT OR REPLACE INTO "texts" VALUES (14936691, 'Worm Erokin', 'This card cannot be Special Summoned. Once per turn, select 1 face-up Reptile-Type "Worm" monster on the field and change it into face-down Defense Position.', '变更表示形式', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (50321796,3,0,47,8225,2300,1400,6,262144,2,24);
+INSERT OR REPLACE INTO "texts" VALUES (50321796, 'Brionac, Dragon of the Ice Barrier', '1 Tuner + 1 or more non-Tuner monsters
+You can discard any number of cards to the Graveyard to target the same number of cards from the field; return them to the hand.', 'Return to hand', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (27315304,3,0,0,8225,2500,1500,9,4096,8,16);
+INSERT OR REPLACE INTO "texts" VALUES (27315304, 'Mist Wurm', '1 Tuner + 2 or more non-Tuner monsters
+When this card is Synchro Summoned, return up to 3 cards your opponent controls to the hand.', 'Return to hand', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (53714009,3,0,44,8225,2100,400,6,128,4,24576);
+INSERT OR REPLACE INTO "texts" VALUES (53714009, 'Flamvell Uruquizas', '1 Tuner + 1 or more non-Tuner monsters
+During battle between this attacking card and a Defense Position monster whose DEF is lower than the ATK of this card, inflict the difference as Battle Damage to your opponent. This card gains 300 ATK each time it inflicts Battle Damage to your opponent.', 'Gain attack', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (80108118,3,0,4109,8225,2200,1300,7,1,1,96);
+INSERT OR REPLACE INTO "texts" VALUES (80108118, 'X-Saber Urbellum', '1 Tuner + 1 or more non-Tuner monsters
+When this card inflicts Battle Damage to your opponent while they have 4 or more cards in their hand, place 1 random card from their hand on top of their Deck.', 'Return to deck', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (26593852,3,0,1,8225,2200,1200,5,32,32,2097154);
+INSERT OR REPLACE INTO "texts" VALUES (26593852, 'Ally of Justice Catastor', '1 Tuner + 1 or more non-Tuner monsters
+At the start of the Damage Step, if this card battles a non-DARK monster: Destroy that monster immediately (without damage calculation).', '暗属性以外怪兽破坏', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (52098461,3,0,9,2,0,0,0,0,0,18);
+INSERT OR REPLACE INTO "texts" VALUES (52098461, 'Wrath of Neos', 'Select 1 face-up "Elemental HERO Neos" you control. Return it to the Deck and destroy all cards on the field.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (89086566,3,0,0,65538,0,0,0,0,0,3);
+INSERT OR REPLACE INTO "texts" VALUES (89086566, 'Detonate', 'Destroy all face-up "Kuriboh" and "Kuriboh Tokens" you control, along with a number of cards your opponent controls, up to the number of "Kuriboh" and "Kuriboh Tokens" destroyed by this effect.', 'Destroy opponent card?', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (15471265,3,0,0,65538,0,0,0,0,0,8192);
+INSERT OR REPLACE INTO "texts" VALUES (15471265, 'Berserker Crush', 'Remove from play 1 monster from your Graveyard. The ATK and DEF of 1 face-up "Winged Kuriboh" you control becomes equal to the ATK and DEF of that monster, until the End Phase of this turn.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (52875873,3,0,0,2,0,0,0,0,0,65539);
+INSERT OR REPLACE INTO "texts" VALUES (52875873, 'Evolution Burst', 'Activate only while you control a face-up "Cyber Dragon"(s). Destroy 1 card your opponent controls. "Cyber Dragon" cannot attack the turn you activate this card.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+COMMIT;
