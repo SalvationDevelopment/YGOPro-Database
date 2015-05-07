4af109208a198504e3a3d1139331dd2f5bb88060
Jamezs Gladney <jamezs@angelofcode.com>
2015-05-06 22:53:15 -0400

    SQL update

---
+++ english/WJ.sql
@@ -0,0 +1,10 @@
+BEGIN TRANSACTION;
+INSERT OR REPLACE INTO "datas" VALUES (50287060,3,0,69,33,2200,2500,6,8,32,8192);
+INSERT OR REPLACE INTO "texts" VALUES (50287060, 'Archfiend of Gilfer', 'When this card is sent to the Graveyard: You can target 1 face-up monster on the field; equip that target with this card. That monster loses 500 ATK while equipped with this card.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (29654737,3,0,4,33,1500,1300,4,1,1,0);
+INSERT OR REPLACE INTO "texts" VALUES (29654737, 'Amazoness Chain Master', 'When this card is destroyed by battle and sent to the Graveyard, you can pay 1500 Life Points to look at your opponent''s hand, select 1 Monster Card in it, and add that card to your hand.', '手牌夺取', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (28649820,3,0,0,131076,0,0,0,0,0,262144);
+INSERT OR REPLACE INTO "texts" VALUES (28649820, 'Embodiment of Apophis', 'During either player''s Main Phase: Special Summon this card as a Normal Monster (Reptile-Type/EARTH/Level 4/ATK 1600/DEF 1800). (This card is also still a Trap Card.)', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT OR REPLACE INTO "datas" VALUES (17484499,3,0,0,4,0,0,0,0,0,128);
+INSERT OR REPLACE INTO "texts" VALUES (17484499, 'Exchange of the Spirit', 'You can only activate 1 “Exchange of the Spirit” per Duel. If each player has 15 or more cards in their Graveyard: Pay 1000 Life Points; both players swaps cards in their Decks with cards in their Graveyards, then shuffle the Decks.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+COMMIT;
---
+++ english/WJC.sql
@@ -0,0 +1,4 @@
+BEGIN TRANSACTION;
+INSERT OR REPLACE INTO "datas" VALUES (89943723,3,0,602120,17,2500,2000,7,1,16,0);
+INSERT OR REPLACE INTO "texts" VALUES (89943723, 'Elemental HERO Neos', 'A new Elemental HERO has arrived from Neo-Space! When he initiates a Contact Fusion with a Neo-Spacian, his unknown powers are unleashed.', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+COMMIT;
